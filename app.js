7d207196cc606078f2e048558b6dd696f5687915
Aji Naufal <aji@skinmystery.com>
2023-09-15 06:55:53 +0700

    feat: app.js

---
+++ app.js
@@ -0,0 +1,18 @@
+const cors = require('cors');
+const dotenv = require('dotenv');
+const express = require('express');
+const mongoose = require('mongoose');
+
+
+dotenv.config();
+const app = express();
+const port = 4000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(cors());
+var corsOptions = { "origin": "*", "methods": "GET,POST" }
+
+app.on('error', (error) => console.error('Server error:', error));
+app.listen(port, () => console.log('Server is running on port ' + port));
